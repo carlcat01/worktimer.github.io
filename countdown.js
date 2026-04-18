ac735ecad03479d68c9b8d7e764adb2374e7e788
Carl Ellingsen <32379479+carlcat01@users.noreply.github.com>
2026-04-18 05:38:19 -0700

    test times

--- countdown.js
+++ countdown.js
@@ -42,3 +42,26 @@ function timeLeftInWeek() {
 console.log(`Week progress: ${getWeekProgress().toFixed(2)}%`);
 
 console.log(timeLeftInWeek());
+
+const weekprogrss = new Date();
+const dtNow = Date.now();
+const nowDay = dtNow.getDay();
+const nowHour = dtNow.getHour()
+const nowMin = dtNow.getMinute(); 
+
+if((nowDay == 3 && nowHour >= 20 )||( nowDay == 4 && (nowHour < 6 || (nowHour == 6 && nowMin <=30)))){
+    //wed-thur shift
+    console.log("wed-thur shift")
+}
+if((nowDay == 4 && nowHour >= 20 )||( nowDay == 5 && (nowHour < 6 || (nowHour == 6 && nowMin <=30)))){
+    //thur-fri shift
+    console.log("thur-fri shift")
+}
+if((nowDay == 5 && nowHour >= 20 )||( nowDay == 6 && (nowHour < 6 || (nowHour == 6 && nowMin <=30)))){
+    //fri-sat shift
+    console.log("fri-sat shift")
+}
+if((nowDay == 6 && nowHour >= 20 )||( nowDay == 0 && (nowHour < 6 || (nowHour == 6 && nowMin <=30)))){
+    //sat-sun shift
+    console.log("sat-sun shift")
+}
